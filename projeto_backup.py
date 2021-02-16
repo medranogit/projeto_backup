cf2d5e6f3c83e45457089a1f76427f52122cf9a8
Vinicius <viniciusmedprofi@hotmail.com>
2021-02-16 18:03:48 -0300

    adicao logica para ficar rodando 24h

--- projeto_backup.py
+++ projeto_backup.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from time import sleep
 import shutil
 
 
@@ -7,16 +8,18 @@ hora_user = input("Coloque a hora da semana nesse formato [12:00]: ")
 
 traducao = {"domingo":"Sunday","segunda":"Monday","terça":"Tuesday","quarta":"Wednesday","quinta":"Thursday","sexta":"Friday","sabado":"Saturday"}
 
-diabackup = datetime.now().strftime("%A")
-horabackup = datetime.now().strftime("%H:%M")
+while True:
+    diabackup = datetime.now().strftime("%A")
+    horabackup = datetime.now().strftime("%H:%M")
+    if traducao[dia_user] == diabackup and hora_user == horabackup:
+        try:
+            shutil.copytree(r'C:\Users\vinim\Documents\backup', r'C:\Users\vinim\Documents\pasta_backup')       
+            print("\nBackup realizado com sucesso\n")
+            sleep(86398)
 
-if traducao[dia_user] == diabackup and hora_user == horabackup:
-    try:
-        shutil.copytree(r'C:\Users\vinim\Documents\backup', r'C:\Users\vinim\Documents\pasta_backup')       
-        print("\nBackup realizado com sucesso\n")
-    except FileExistsError as e:
-        shutil.rmtree(r'C:\Users\vinim\Documents\pasta_backup')
-        shutil.copytree(r'C:\Users\vinim\Documents\backup', r'C:\Users\vinim\Documents\pasta_backup')  
-        print("\nBackup realizado com sucesso\n")
-else:
-    print("Não realizar backup")
+        except FileExistsError as e:
+            shutil.rmtree(r'C:\Users\vinim\Documents\pasta_backup')
+            shutil.copytree(r'C:\Users\vinim\Documents\backup', r'C:\Users\vinim\Documents\pasta_backup')  
+            print("\nBackup realizado com sucesso\n")
+            sleep(86398)
+    
