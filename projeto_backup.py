c2dcfe3d7ed0f1e7311cbb71bcb6ea630740bb3f
Vinicius <viniciusmedprofi@hotmail.com>
2021-02-15 22:13:08 -0300

    update no codigo

--- projeto_backup.py
+++ projeto_backup.py
@@ -1,34 +1,22 @@
-#! /usr/bin/python
-from datetime import datetime, timedelta, date
+from datetime import datetime
 import shutil
-import os, os.path, subprocess
+
 
 dia_user = input("Coloque o dia da semana do backup: ").lower()
 hora_user = input("Coloque a hora da semana nesse formato [12:00]: ")
 
-traducao = {"sabado":"Saturday"}
+traducao = {"domingo":"Sunday","segunda":"Monday","terça":"Tuesday","quarta":"Wednesday","quinta":"Thursday","sexta":"Friday","sabado":"Saturday"}
 
 diabackup = datetime.now().strftime("%A")
-
 horabackup = datetime.now().strftime("%H:%M")
 
 if traducao[dia_user] == diabackup and hora_user == horabackup:
-
-    pastabackup = r'C:\Users\vinim\Documents\backup'
-    localbackup = r'C:\Users\vinim\Documents\pasta_do_backup'
-
-
-
-    print("Backup Completo")
-
-    print("Backup realizado com sucesso")
+    try:
+        shutil.copytree(r'C:\Users\vinim\Documents\backup', r'C:\Users\vinim\Documents\pasta_backup')       
+        print("\nBackup realizado com sucesso\n")
+    except FileExistsError as e:
+        shutil.rmtree(r'C:\Users\vinim\Documents\pasta_backup')
+        shutil.copytree(r'C:\Users\vinim\Documents\backup', r'C:\Users\vinim\Documents\pasta_backup')  
+        print("\nBackup realizado com sucesso\n")
 else:
     print("Não realizar backup")
-
-
-
-
-
-
-
-
